8cdf3cb95e9c245a47912159a86e0348bc2095e8
Pedro Lopes <lopesphls@gmail.com>
2022-11-11 00:49:45 -0300

    A

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -11,45 +11,43 @@ interface IPosts {
 	userId: string
 }
 
-const Api = axios.create({ baseURL: 'https://foi.onrender.com/' })
-
 const Home = () => {
 	const [posts, setPosts] = useState<IPosts[]>([])
 	const [post, setPost] = useState('')
 	const [atualizar, setAtualizar] = useState(false)
 
+	const Api = axios.create({ baseURL: 'https://foi.onrender.com/' })
 	async function Create(event) {
 		event.preventDefault()
-		const { title } = event.target.elements
+		const { message } = event.target.elements
 		console.log(event)
-		await Api.post('/posts/create', {
-			title: post,
+		await Api.post('posts/create', {
+			title: message.value,
 		})
 		setAtualizar(value => !value)
 	}
 
-	async function Edit(id, check) {
-		await Api.put(`/posts/edit/${id}`, {
-			check: !check,
+	async function Edit(id, checked) {
+		await Api.put(`posts/edit/${id}`, {
+			checked: !checked,
 		})
 		setAtualizar(value => !value)
 	}
 
 	async function Delete(id) {
-		await Api.delete(`/posts/delete/${id}`)
+		await Api.delete(`posts/delete/${id}`)
 		setAtualizar(value => !value)
 	}
 
 	useEffect(() => {
 		async function getAll() {
-			const response = await axios
-				.get('https://foi.onrender.com/posts/')
-				.then(res => {
-					return res.data
-				})
+			const response = await Api.get('posts/').then(res => {
+				return res.data
+			})
 			setPosts(response)
 		}
-	}, [atualizar])
+		getAll()
+	}, [])
 
 	return (
 		<div className='home'>
@@ -68,33 +66,37 @@ const Home = () => {
 				</div>
 			</form>
 
-			{posts.map(el => (
-				<div
-					key={el.id}
-					className='border-2 border-black rounded-3xl h-auto bg-white flex flex-row items-center justify-between mb-2 drop-shadow-xl'
-				>
-					<input
-						checked={el.checked}
-						onChange={() => Edit(el.id, el.checked)}
-						type='checkbox'
-						className='ml-4 w-5 hover:bg-red-400 checked:bg-green-400'
-						id=''
-					/>
-					<h1 className='text-black p-5 text-1xl rounded-lg'>{el.title}</h1>
-					<div>
-						<button type='button' className='mr-4 text-black'>
-							<BiPencil />
-						</button>
-						<button
-							type='button'
-							onClick={() => Delete(el.id)}
-							className='mr-4 text-black'
-						>
-							<BiTrash />
-						</button>
+			{posts.map(el =>
+				!el.checked ? (
+					<div
+						key={el.id}
+						className='border-2 border-black rounded-3xl h-auto bg-white flex flex-row items-center justify-between mb-2 drop-shadow-xl'
+					>
+						<input
+							checked={el.checked}
+							onChange={() => Edit(el.id, el.checked)}
+							type='checkbox'
+							className=''
+							id=''
+						/>
+						<h1 className='text-black p-5 text-1xl rounded-lg'>{el.title}</h1>
+						<div>
+							<button type='button' className='mr-4 text-black'>
+								<BiPencil />
+							</button>
+							<button
+								type='button'
+								onClick={() => Delete(el.id)}
+								className='mr-4 text-black'
+							>
+								<BiTrash />
+							</button>
+						</div>
 					</div>
-				</div>
-			))}
+				) : (
+					<div />
+				),
+			)}
 		</div>
 	)
 }
